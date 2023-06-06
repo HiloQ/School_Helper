d027024a7a7b1b667e43d93e2749bea712c2f580
Jakub Sukiennik <lordquick@icloud.com>
2023-06-06 19:28:30 +0200

    Initial Commit

---
+++ to_do_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  to_do_list
+//
+//  Created by Jakub Sukiennim on 06/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
